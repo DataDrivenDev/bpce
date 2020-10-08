ac4d8c2022fac89c2cf661060f22115d5c4c4971
dataDrivenDev <cedric.lautard@data-driven.fr>
2020-10-08 11:34:46 +0200

    config for deploying

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,14 @@
 module.exports = {
-
-  apps : [{
-    script: './pages/index.tsx',
-    watch: '.',
-    environement: {
-      NODE_ENV: "development",
-    }
-  },{
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
   deploy : {
-    production : {
+    development : {
       key :  '~/.ssh/id_rsa',
       user: 'datadriven-user',
       host : 'esxvm-vcenter1-web-datadriven1.synten.com',
-      ref  : 'origin/bpce',
-      repo : 'git@github.com:DataDrivenDev/DashRecruteur.git',
+      ref  : 'origin/develop',
+      repo : 'git@github.com:DataDrivenDev/bpce.git',
       path : '/home/www/dev/www/siteCarriere/bpce',
       "pre-setup" : "rm -rf /home/www/dev/www/siteCarriere/bpce",
-      "post-setup" : 'npm install && npm build',
-      env  : {
-        "NODE_ENV": "development"
-      }
+      "post-setup" : 'npm install && npm run build'
     }
   }
 };
