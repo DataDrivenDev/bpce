7a6580482c12b3e143f9bb89a54e97085fc899df
dataDrivenDev <cedric.lautard@data-driven.fr>
2020-10-11 18:17:08 +0200

    config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,10 @@ module.exports = {
       key :  '~/.ssh/id_rsa',
       user: 'datadriven-user',
       host : 'esxvm-vcenter1-web-datadriven1.synten.com',
-      ref  : 'origin/develop',
+      ref  : 'origin/master',
       repo : 'git@github.com:DataDrivenDev/bpce.git',
-      path : '/home/www/dev/www/siteCarriere/bpce',
-      "pre-setup" : "rm -rf /home/www/dev/www/siteCarriere/bpce",
+      path : '/home/www/clients/www/siteCarriere/bpce',
+      "pre-setup" : "rm -rf /home/www/clients/www/siteCarriere/bpce",
       "post-setup" : 'npm install && npm run build'
     }
   }
